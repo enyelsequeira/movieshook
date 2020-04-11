2ddbc2aef4018908391bf92e60b03ea2cfc0155f
unknown <enyelsequeira@hotmail.com>
2020-04-11 21:26:04 +0100

    new commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-const url ='https://api.themoviedb.org/3/movie/popular?api_key=4e0d07555e20e0345f6bd12869b2604e&language=en-US&page=1'
+const url = 'https://api.themoviedb.org/3/movie/popular?api_key=4e0d07555e20e0345f6bd12869b2604e&language=en-US&page=1';
 
 export const fetchMovies = async () => {
-    try {
-        const { data: { results } } = await axios.get(url);
+  try {
+    const { data: { results } } = await axios.get(url);
 
-        return results;
-    } catch (error) {
-        
-    }
-}
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
