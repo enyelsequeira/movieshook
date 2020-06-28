9dac54bcefc668052546023e7eb0d09b28412187
enyelsequeira <enyelsequeira@hotmail.com>
2020-06-28 20:36:39 +0100

    css is responsive now

--- src/components/movies/Movie/Movie.jsx
+++ src/components/movies/Movie/Movie.jsx
@@ -7,13 +7,13 @@ import { Link } from 'react-router-dom';
 import styles from './Movie.module.scss';
 
 const Movie = ({ movie, i, handleOpen }) => (
-  <Grid item xs={12} sm={4} md={2} lg={2} onClick={() => handleOpen(movie)} className={styles.movie} key={i}>
+  <div item xs={12} sm={6} md={4} lg={4} spacing={4} onClick={() => handleOpen(movie)} className={styles.movie} key={i}>
     <Link to="/movie">
       <img alt={movie.title} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
       <h4>{movie.title}</h4>
       <StarRatings rating={movie.vote_average / 2} numberOfStars={5} starDimension="20px" starSpacing="2px" className={styles.ratings} />
     </Link>
-  </Grid>
+  </div>
 );
 
 export default Movie;
