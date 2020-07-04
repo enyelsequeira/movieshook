fd339c26b4ae7927def91de52c0985fecad7eafe
enyelsequeira <enyelsequeira@hotmail.com>
2020-07-04 01:12:32 +0100

    movid cards changes

--- src/components/movies/Movie/Movie.jsx
+++ src/components/movies/Movie/Movie.jsx
@@ -8,7 +8,7 @@ import styles from './Movie.module.scss';
 
 const Movie = ({ movie, i, handleOpen }) => (
   <div item xs={12} sm={6} md={4} lg={4} spacing={4} onClick={() => handleOpen(movie)} className={styles.movie} key={i}>
-    <Link to="/movie">
+    <Link className={styles.links} to="/movie">
       <img alt={movie.title} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
       <h4>{movie.title}</h4>
       <StarRatings rating={movie.vote_average / 2} numberOfStars={5} starDimension="20px" starSpacing="2px" className={styles.ratings} />
